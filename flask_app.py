7d125f661dfaf8714eb2217ffe0a3005b46f0afb
Sefa Aydinelli <sefaydinelli@hotmail.com>
2021-04-24 17:13:36 +0300

    /select endpointi düzenlendi ve hatalar giderildi.

--- flask_app.py
+++ flask_app.py
@@ -23,165 +23,17 @@ def mysql_connect():
 def mysql_errors(e):
     if(e.errno == errorcode.ER_ACCESS_DENIED_ERROR):
         logging.error(str(e))
-        return make_response(jsonify(Success = False, ERROR = 'AUTH ERROR! CHECK YOUR LOG FILE FOR MORE INFO!'), 401)
+        return ({"Success" : "False", "ERROR" : "AUTH ERROR! CHECK YOUR LOG FILE FOR MORE INFO!"})
     elif(e.errno == errorcode.ER_BAD_DB_ERROR):
         logging.error(str(e))
-        return make_response(jsonify(Success = False, ERROR = 'DB DOES NOT EXIST! CHECK YOUR LOG FILE FOR MORE INFO!'), 404)
+        return ({"Success" : "False", "ERROR" : "DB DOES NOT EXIST! CHECK YOUR LOG FILE FOR MORE INFO!"})
     elif(e.errno == errorcode.ER_BAD_TABLE_ERROR):
         logging.error(str(e))
-        return make_response(jsonify(Success = False, ERROR = 'TABLE DOES NOT EXIST! CHECK YOUR LOG FILE FOR MORE INFO!'), 404)
+        return ({"Success" : "False", "ERROR" : "TABLE DOES NOT EXIST! CHECK YOUR LOG FILE FOR MORE INFO!"})
     else:
         logging.error(str(e))
-        return make_response(jsonify(Success = False, ERROR = "UNKNOWN ERROR! CHECK YOUR LOG FILE FOR MORE INFO."), 404)
+        return ({"Success" : "False", "ERROR" : "UNKNOWN ERROR! CHECK YOUR LOG FILE FOR MORE INFO."})
 
-def mysql_insert(table_name, firstname, lastname, email):
-    try:
-        mysqldb = mysql_connect()
-        cursor = mysqldb.cursor(buffered=True)
-        query = f""" INSERT INTO 
-        {config['DB']['mysql_database']}.{table_name}
-        ( firstname, lastname, email ) VALUES 
-        ( '{firstname}', '{lastname}', '{email}') """
-        cursor.execute(query)
-        mysqldb.commit()
-        mysqldb.close()
-        return make_response(jsonify(Success=True, Status=f"NEW VALUES ADDED TO {table_name}"), 201)
-    except mysql.connector.Error as e:
-        return(mysql_errors(e))
-
-def mysql_select(table_name, id_obj, firstname, lastname, email):
-    # Sadece table_name verilirse.
-    if (firstname == None and lastname == None and email == None and id_obj == None):
-        try:
-            mysqldb = mysql_connect()
-            cursor = mysqldb.cursor(buffered=True)
-            query = f""" SELECT * FROM
-            {config['DB']['mysql_database']}.{table_name} """
-            cursor.execute(query)
-            row_headers=[x[0] for x in cursor.description]
-            response = cursor.fetchall()
-            json_data=[]
-            for result in response:
-                json_data.append(dict(zip(row_headers, result)))
-            mysqldb.close()
-            if json_data == []:
-                return make_response(jsonify(Success=False, Status="Aradığınız veri bulunamadı!"), 204)
-            else:
-                return make_response(jsonify(json_data), 200)
-        except mysql.connector.Error as e:
-            return(mysql_errors(e))
-    
-    # table_name ve email verilirse.
-    elif (firstname == None and lastname == None and id_obj == None):
-        try:
-            mysqldb = mysql_connect()
-            cursor = mysqldb.cursor(buffered=True)
-            query = f""" SELECT * FROM
-            {config['DB']['mysql_database']}.{table_name} 
-            WHERE email = '{email}'"""
-            cursor.execute(query)
-            row_headers=[x[0] for x in cursor.description]
-            response = cursor.fetchall()
-            json_data=[]
-            for result in response:
-                json_data.append(dict(zip(row_headers, result)))
-            mysqldb.close()
-            if json_data == []:
-                return make_response(jsonify(Success=False, Status="Aradığınız veri bulunamadı!"), 204)
-            else:
-                return make_response(jsonify(json_data), 200)
-        except mysql.connector.Error as e:
-            return(mysql_errors(e))
-    
-    # table_name ve id verilirse
-    elif (firstname == None and lastname == None and email == None):
-        try:
-            mysqldb = mysql_connect()
-            cursor = mysqldb.cursor(buffered=True)
-            query = f""" SELECT * FROM
-            {config['DB']['mysql_database']}.{table_name} 
-            WHERE id = '{id_obj}'"""
-            cursor.execute(query)
-            row_headers=[x[0] for x in cursor.description]
-            response = cursor.fetchall()
-            json_data=[]
-            for result in response:
-                json_data.append(dict(zip(row_headers, result)))
-            mysqldb.close()
-            if json_data == []:
-                return make_response(jsonify(Success=False, Status="Aradığınız veri bulunamadı!"), 204)
-            else:
-                return make_response(jsonify(json_data), 200)
-        except mysql.connector.Error as e:
-            return(mysql_errors(e))
-    
-    # table_name ve firstname verilirse
-    elif (lastname == None and email == None and id_obj == None):
-        try:
-            mysqldb = mysql_connect()
-            cursor = mysqldb.cursor(buffered=True)
-            query = f""" SELECT * FROM
-            {config['DB']['mysql_database']}.{table_name} 
-            WHERE firstname = '{firstname}'"""
-            cursor.execute(query)
-            row_headers=[x[0] for x in cursor.description]
-            response = cursor.fetchall()
-            json_data=[]
-            for result in response:
-                json_data.append(dict(zip(row_headers, result)))
-            mysqldb.close()
-            if json_data == []:
-                return make_response(jsonify(Success=False, Status="Aradığınız veri bulunamadı!"), 204)
-            else:
-                return make_response(jsonify(json_data), 200)
-        except mysql.connector.Error as e:
-            return(mysql_errors(e))
-
-    # table_name ve lastname verilirse
-    elif (firstname == None and email == None and id_obj == None):
-        try:
-            mysqldb = mysql_connect()
-            cursor = mysqldb.cursor(buffered=True)
-            query = f""" SELECT * FROM
-            {config['DB']['mysql_database']}.{table_name} 
-            WHERE lastname = '{lastname}'"""
-            cursor.execute(query)
-            row_headers=[x[0] for x in cursor.description]
-            response = cursor.fetchall()
-            json_data=[]
-            for result in response:
-                json_data.append(dict(zip(row_headers, result)))
-            mysqldb.close()
-            if json_data == []:
-                return make_response(jsonify(Success=False, Status="Aradığınız veri bulunamadı!"), 204)
-            else:
-                return make_response(jsonify(json_data), 200)
-        except mysql.connector.Error as e:
-            return(mysql_errors(e))
-    
-    # table_name, firstname ve lastname verilirse
-    elif (email == None and id_obj == None):
-        try:
-            mysqldb = mysql_connect()
-            cursor = mysqldb.cursor(buffered=True)
-            query = f""" SELECT * FROM
-            {config['DB']['mysql_database']}.{table_name} 
-            WHERE firstname = '{firstname}' AND lastname = '{lastname}'"""
-            cursor.execute(query)
-            row_headers=[x[0] for x in cursor.description]
-            response = cursor.fetchall()
-            json_data=[]
-            for result in response:
-                json_data.append(dict(zip(row_headers, result)))
-            mysqldb.close()
-            if json_data == []:
-                return make_response(jsonify(Success=False, Status="Aradığınız veri bulunamadı!"), 204)
-            else:
-                return make_response(jsonify(json_data), 200)
-        except mysql.connector.Error as e:
-            return(mysql_errors(e))
-    else:
-        return make_response(jsonify(Success=False, Status="Aradığınız veri bulunamadı."), 204)    
 @app.route('/connect', methods = ['GET'])
 def connect():
     try:
@@ -198,8 +50,142 @@ def select():
     firstname = request.args.get("firstname")
     lastname = request.args.get("lastname")
     email = request.args.get("email")
-    response = mysql_select(table_name, id_obj, firstname, lastname, email)
-    return response
+    
+    #sadece table_name verilirse
+    if (firstname == None and lastname == None and email == None and id_obj == None and table_name != None):
+        try:
+            mysqldb = mysql_connect()
+            cursor = mysqldb.cursor(buffered=True)
+            query = f""" SELECT * FROM
+            {config['DB']['mysql_database']}.{table_name} """
+            cursor.execute(query)
+            row_headers=[x[0] for x in cursor.description]
+            response = cursor.fetchall()
+            json_data=[]
+            for result in response:
+                json_data.append(dict(zip(row_headers, result)))
+            mysqldb.close()
+            if json_data == []:
+                return make_response(jsonify(Success=False, Status="Aradığınız veri bulunamadı!"), 404)
+            else:
+                return make_response(jsonify(json_data), 200)
+        except mysql.connector.Error as e:
+            return make_response(jsonify(Success = False, Status = f"{table_name} isminde bir tablo bulunamadi!"), 404)
+    
+    # table_name ve email verilirse.
+    elif (firstname == None and lastname == None and id_obj == None and table_name != None):
+        try:
+            mysqldb = mysql_connect()
+            cursor = mysqldb.cursor(buffered=True)
+            query = f""" SELECT * FROM
+            {config['DB']['mysql_database']}.{table_name} 
+            WHERE email = '{email}'"""
+            cursor.execute(query)
+            row_headers=[x[0] for x in cursor.description]
+            response = cursor.fetchall()
+            json_data=[]
+            for result in response:
+                json_data.append(dict(zip(row_headers, result)))
+            mysqldb.close()
+            if json_data == []:
+                return make_response(jsonify(Success=False, Status="Aradığınız veri bulunamadı!"), 404)
+            else:
+                return make_response(jsonify(json_data), 200)
+        except mysql.connector.Error as e:
+            return make_response(jsonify(mysql_errors(e)), 404)
+    
+    # table_name ve id verilirse
+    elif (firstname == None and lastname == None and email == None and table_name != None):
+        try:
+            mysqldb = mysql_connect()
+            cursor = mysqldb.cursor(buffered=True)
+            query = f""" SELECT * FROM
+            {config['DB']['mysql_database']}.{table_name} 
+            WHERE id = '{id_obj}'"""
+            cursor.execute(query)
+            row_headers=[x[0] for x in cursor.description]
+            response = cursor.fetchall()
+            json_data=[]
+            for result in response:
+                json_data.append(dict(zip(row_headers, result)))
+            mysqldb.close()
+            if json_data == []:
+                return make_response(jsonify(Success=False, Status="Aradığınız veri bulunamadı!"), 404)
+            else:
+                return make_response(jsonify(json_data), 200)
+        except mysql.connector.Error as e:
+            return make_response(jsonify(mysql_errors(e)), 404)
+    
+    # table_name ve firstname verilirse
+    elif (lastname == None and email == None and id_obj == None and table_name != None):
+        try:
+            mysqldb = mysql_connect()
+            cursor = mysqldb.cursor(buffered=True)
+            query = f""" SELECT * FROM
+            {config['DB']['mysql_database']}.{table_name} 
+            WHERE firstname = '{firstname}'"""
+            cursor.execute(query)
+            row_headers=[x[0] for x in cursor.description]
+            response = cursor.fetchall()
+            json_data=[]
+            for result in response:
+                json_data.append(dict(zip(row_headers, result)))
+            mysqldb.close()
+            if json_data == []:
+                return make_response(jsonify(Success=False, Status="Aradığınız veri bulunamadı!"), 404)
+            else:
+                return make_response(jsonify(json_data), 200)
+        except mysql.connector.Error as e:
+            return make_response(jsonify(mysql_errors(e)), 404)
+
+    # table_name ve lastname verilirse
+    elif (firstname == None and email == None and id_obj == None and table_name != None):
+        try:
+            mysqldb = mysql_connect()
+            cursor = mysqldb.cursor(buffered=True)
+            query = f""" SELECT * FROM
+            {config['DB']['mysql_database']}.{table_name} 
+            WHERE lastname = '{lastname}'"""
+            cursor.execute(query)
+            row_headers=[x[0] for x in cursor.description]
+            response = cursor.fetchall()
+            json_data=[]
+            for result in response:
+                json_data.append(dict(zip(row_headers, result)))
+            mysqldb.close()
+            if json_data == []:
+                return make_response(jsonify(Success=False, Status="Aradığınız veri bulunamadı!"), 404)
+            else:
+                return make_response(jsonify(json_data), 200)
+        except mysql.connector.Error as e:
+            return make_response(jsonify(mysql_errors(e)), 404)
+    
+    # table_name, firstname ve lastname verilirse
+    elif (email == None and id_obj == None and table_name != None):
+        try:
+            mysqldb = mysql_connect()
+            cursor = mysqldb.cursor(buffered=True)
+            query = f""" SELECT * FROM
+            {config['DB']['mysql_database']}.{table_name} 
+            WHERE firstname = '{firstname}' AND lastname = '{lastname}'"""
+            cursor.execute(query)
+            row_headers=[x[0] for x in cursor.description]
+            response = cursor.fetchall()
+            json_data=[]
+            for result in response:
+                json_data.append(dict(zip(row_headers, result)))
+            mysqldb.close()
+            if json_data == []:
+                return make_response(jsonify(Success=False, Status="Aradığınız veri bulunamadı!"), 404)
+            else:
+                return make_response(jsonify(json_data), 200)
+        except mysql.connector.Error as e:
+            return make_response(jsonify(mysql_errors(e)), 404)
+    # table_name verilmezse
+    elif (table_name == None and firstname == None and lastname == None and email == None and id_obj == None):
+        return make_response(jsonify(Success=False, Status="Tablo ismi belirtilmedi."), 404)
+    else:
+        return make_response(jsonify(Success=False, Status="Aradığınız veri bulunamadı.."), 404)
 
 @app.route('/insert', methods = ['POST', 'PUT'])
 def insert():
@@ -210,14 +196,38 @@ def insert():
         firstname = request.args.get("firstname")
         lastname = request.args.get("lastname")
         email = request.args.get("email")
-        return(mysql_insert(table_name,firstname,lastname,email))
+        try:
+            mysqldb = mysql_connect()
+            cursor = mysqldb.cursor(buffered=True)
+            query = f""" INSERT INTO 
+            {config['DB']['mysql_database']}.{table_name}
+            ( firstname, lastname, email ) VALUES 
+            ( '{firstname}', '{lastname}', '{email}') """
+            cursor.execute(query)
+            mysqldb.commit()
+            mysqldb.close()
+            return make_response(jsonify(Success=True, Status=f"NEW VALUES ADDED TO {table_name}"), 201)
+        except mysql.connector.Error as e:
+            return make_response(jsonify(mysql_errors(e)), 404)
     elif req_methods == 'PUT': #json formatında al.
         js_object = request.get_json()
         table_name = js_object["table_name"]
         firstname = js_object["firstname"]
         lastname = js_object["lastname"]
         email = js_object["email"]
-        return(mysql_insert(table_name,firstname,lastname,email))
+        try:
+            mysqldb = mysql_connect()
+            cursor = mysqldb.cursor(buffered=True)
+            query = f""" INSERT INTO 
+            {config['DB']['mysql_database']}.{table_name}
+            ( firstname, lastname, email ) VALUES 
+            ( '{firstname}', '{lastname}', '{email}') """
+            cursor.execute(query)
+            mysqldb.commit()
+            mysqldb.close()
+            return make_response(jsonify(Success=True, Status=f"NEW VALUES ADDED TO {table_name}"), 201)
+        except mysql.connector.Error as e:
+            return make_response(jsonify(mysql_errors(e)), 404)
     else:
         return make_response(jsonify(ERROR=f"{req_methods} METHOD IS NOT ALLOWED!"), 405)
 
